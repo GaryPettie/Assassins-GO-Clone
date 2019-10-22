af1e0eca4522db819f16c5c9c69cfab62f781097
Gary Pettie <garypettie@gmail.com>
2019-10-22 10:30:07 +0100

    Added new level geometry for sample scene. Added LevelName text to the scene (displays on the front plaque)

---
+++ Assets/DisplayLevelName.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class DisplayLevelName : MonoBehaviour
+{
+	[SerializeField] TextMeshProUGUI label;
+
+	void Awake () {
+		if (label != null) {
+			label.text = SceneManager.GetActiveScene().name;
+		}
+	}
+}
